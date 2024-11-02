67bdf515bdcf208af42b3e4b02449d3d7aee019d
williamtran207 <quochuyphbrvt07@gmail.com>
2024-11-02 21:53:02 +0700

    update

--- src/pages/services/GlobalApi.js
+++ src/pages/services/GlobalApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'https://openai-api-nodejs-8fa5642a8d8d.herokuapp.com/app'
+const BASE_URL = 'https://nodejs-api-86f02c990620.herokuapp.com/app'
 // const BASE_URL = 'http://192.168.1.21:3001/app'   //Replace with System PC IP address
 
 const getBardApi = (msg) => axios.get(BASE_URL + "?ques=" + msg)
